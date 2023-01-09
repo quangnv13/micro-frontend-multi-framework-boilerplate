534b9e8d9dc74bcce3c0ed69452ea856959d9b06
quangnv13 <quangnv1311@gmail.com>
2023-01-09 10:47:23 +0700

    update install dependencies

---
+++ concurrently_install.js
@@ -0,0 +1,44 @@
+const concurrently = require("concurrently");
+const path = require("path");
+const packageManager = process.env.PM || "npm";
+const { result } = concurrently([
+  {
+    command: `${packageManager} install`,
+    name: "assets-server",
+    cwd: path.resolve(__dirname, "share/assets"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "solid-shell",
+    cwd: path.resolve(__dirname, "solid-shell"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "solid-about",
+    cwd: path.resolve(__dirname, "solid-about"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "angular-contact",
+    cwd: path.resolve(__dirname, "angular-contact"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "react-home",
+    cwd: path.resolve(__dirname, "react-home"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "svelte-product",
+    cwd: path.resolve(__dirname, "svelte-product"),
+  },
+  {
+    command: `${packageManager} install`,
+    name: "vue-cart",
+    cwd: path.resolve(__dirname, "vue-cart"),
+  },
+]);
+result.then(
+  () => console.log("******* Start all projects successed *******"),
+  (err) => console.error(err)
+);
